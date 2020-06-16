c623d34bedbbe17466b706d632d769717d6e9a4b
suyun <sarbayev.s@chocofood.kz>
2020-06-16 20:50:02 +0600

    Added send sms implementation

--- app/src/main/java/kz/education/stepeducation/MainActivity.kt
+++ app/src/main/java/kz/education/stepeducation/MainActivity.kt
@@ -1,14 +1,19 @@
 package kz.education.stepeducation
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.telephony.SmsManager
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -66,7 +71,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         Log.d("LIFE", "onRestoreInstanceState")
-        Log.d("MESSAGE", savedInstanceState.getString("MESSAGE") ?: "")
+        Log.d("LIFE", savedInstanceState.getString("MESSAGE") ?: "")
         super.onRestoreInstanceState(savedInstanceState)
     }
 
@@ -84,12 +89,53 @@ class MainActivity : AppCompatActivity() {
         textviewStatus = findViewById(R.id.textview_activity_main_status)
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     fun initializeListeners(){
         buttonAction!!.setOnClickListener(View.OnClickListener {
-            initiateStudentsActivityTransition()
+            if(initiateCheckPermission()){
+                initiateSendSMS("77081170832")
+            }else{
+                initiateRequestPermission()
+            }
+//            initiateStudentsActivityTransition()
 //            textviewStatus!!.setText(getString(R.string.acitvity_main_textview_status_pressed))
 //            textviewStatus!!.setTextColor(ContextCompat.getColor(this, R.color.colorAccent))
         })
+    }
+
+    fun initiateSendSMS(phone: String){
+        var smsManager = SmsManager.getDefault()
+        smsManager.sendTextMessage(phone, null,"sms-message", null,null)
+//        var smsIntent = Intent(Intent.ACTION_VIEW)
+//        smsIntent.putExtra("sms body", "default-content")
+//        smsIntent.setType("vnd.android-dir/mms-sms")
+//        startActivity(smsIntent)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun initiateRequestPermission(){
+        requestPermissions(Array<String>(1){android.Manifest.permission.SEND_SMS}, 101)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun initiateCheckPermission() : Boolean{
+        return checkSelfPermission(android.Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            101 -> {
+                if(grantResults.size >= 1){
+                    initiateSendSMS("77081170832")
+                }
+            }
+        }
     }
 
     fun initiateStudentsActivityTransition(){
