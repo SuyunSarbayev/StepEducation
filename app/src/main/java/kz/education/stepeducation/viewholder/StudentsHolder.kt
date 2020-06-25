42e273ff0fd17e8abe1f3639380898a2f70fadb0
suyun <sarbayev.s@chocofood.kz>
2020-06-25 19:56:26 +0600

    Added String resources with variable

--- app/src/main/java/kz/education/stepeducation/viewholder/StudentsHolder.kt
+++ app/src/main/java/kz/education/stepeducation/viewholder/StudentsHolder.kt
@@ -1,5 +1,6 @@
 package kz.education.stepeducation.viewholder
 
+import android.content.Context
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -7,6 +8,8 @@ import kz.education.stepeducation.R
 import kz.education.stepeducation.data.Student
 
 class StudentsHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    var context: Context? = null
 
     var textViewName: TextView? = null
     var textViewDescription: TextView? = null
@@ -16,6 +19,7 @@ class StudentsHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     }
 
     fun initializeViews(){
+        textViewName.setText(context.getString(R.string.viewholder_student_name_header, "Kirill"))
         textViewName = itemView.findViewById(R.id.textview_viewholder_student_name)
         textViewDescription = itemView.findViewById(R.id.textview_viewholder_student_description)
     }
